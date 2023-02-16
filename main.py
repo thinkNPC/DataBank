f435c372aab0c3bf5ae8d479eb902552934ad612
Robbie Sinclair <rcsinclair3@gmail.com>
2023-02-16 14:40:26 +0000

    remove main obselet functions

--- main.py
+++ main.py
@@ -14,25 +14,12 @@ def run_databank():
     Output(DataBank).csv()
 
 
-def run_one(asset):
-    print(asset)
-    df = asset.get_data()
-    print(df)
-
-
 def run_all():
     for asset in DATA_BANK_INPUTS:
         print(asset)
         df = asset.get_data()
         logging.info(asset)
         logging.info(df.iloc[0].head())
-
-
-def report(fname):
-    with open(fname, "w") as f:
-        f.write("# Example DataBank report\n\n")
-        for asset in REPORT:
-            f.write(Output(asset).to_md_str())
 
 
 if __name__ == "__main__":
